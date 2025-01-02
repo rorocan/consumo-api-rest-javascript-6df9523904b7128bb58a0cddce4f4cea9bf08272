d5206d5863853c1253a85ea16430956894a1d1f8
Rodrigo Rojas <rorocanrojas1998@gmail.com>
2025-01-02 10:48:09 -0500

    principales

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const API_URL_RANDOM = 'https://api.thecatapi.com/v1/images/search?limit=2&api_key=c08d415f-dea7-4a38-bb28-7b2188202e46';
-const API_URL_FAVOTITES = 'https://api.thecatapi.com/v1/favourites?limit=2&api_key=c08d415f-dea7-4a38-bb28-7b2188202e46';
+const API_URL_FAVOTITES = 'https://api.thecatapi.com/v1/favourites?api_key=c08d415f-dea7-4a38-bb28-7b2188202e46';
 
 const spanError = document.getElementById('error')
 
@@ -14,13 +14,18 @@ async function loadRandomMichis() {
   } else {
     const img1 = document.getElementById('img1');
     const img2 = document.getElementById('img2');
+    const btn1 = document.getElementById('btn1');
+    const btn2 = document.getElementById('btn2');
     
     img1.src = data[0].url;
     img2.src = data[1].url;
+
+    btn1.onclick = () => saveFavouriteMichi(data[0].id);
+    btn2.onclick = () => saveFavouriteMichi(data[1].id);
   }
 }
 
-async function loadFavoritesMichis() {
+async function loadFavouriteMichis() {
   const res = await fetch(API_URL_FAVOTITES);
   const data = await res.json();
   console.log('Favoritos')
@@ -28,8 +33,43 @@ async function loadFavoritesMichis() {
 
   if (res.status !== 200) {
     spanError.innerHTML = "Hubo un error: " + res.status + data.message;
+  } else {
+    data.forEach(michi => {
+      const section = document.getElementById('favoriteMichis')
+      const article = document.createElement('article');
+      const img = document.createElement('img');
+      const btn = document.createElement('button');
+      const btnText = document.createTextNode('Sacar al michi de favoritos');
+
+      img.src = michi.image.url;
+      img.width = 150;
+      btn.appendChild(btnText);
+      article.appendChild(img);
+      article.appendChild(btn);
+      section.appendChild(article);
+    });
+  }
+}
+
+async function saveFavouriteMichi(id) {
+  const res = await fetch(API_URL_FAVOTITES, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      image_id: id
+    }),
+  });
+  const data = await res.json();
+
+  console.log('Save')
+  console.log(res)
+
+  if (res.status !== 200) {
+    spanError.innerHTML = "Hubo un error: " + res.status + data.message;
   }
 }
 
 loadRandomMichis();
-loadFavoritesMichis();
+loadFavouriteMichis();
